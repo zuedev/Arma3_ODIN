2c943e8e2c1280b38e1305684d2266a1973ee778
Alex <zuedev@gmail.com>
2024-07-06 11:15:33 +0100

    fixes

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -1,9 +1,11 @@
 $PublisherCMD = "C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\Publisher\PublisherCmd.exe"
 
-Write-Host "Running HEMTT Release" & hemtt release --no-archive --expsqfc
+Write-Host "Running HEMTT Release"
+& hemtt release --no-archive --expsqfc
 
 if (Test-Path "steam\workshop_id.txt") {
     $workshopId = Get-Content "steam\workshop_id.txt"
 
-    Write-Host "Running Arma 3 Tools PublisherCmd" & $PublisherCMD update /id:$workshopId /path:.hemttout\release\ /changeNote:"automated release"
+    Write-Host "Running Arma 3 Tools PublisherCmd"
+    & $PublisherCMD update /id:$workshopId /path:.hemttout\release\ /changeNote:"automated release"
 }
